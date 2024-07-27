25b3d2a88cc90f991f82ffa4737d5911baa4a35c
krishna-bhardwaj-1 <krishnabhardwaj5427912@gmail.com>
2024-07-27 16:55:20 +0530

    File added Successfully

---
+++ src/oops/method.java
@@ -0,0 +1,7 @@
+package oops;
+/*  A method is a collection of statements. must have return type.
+    Java does not support Nested method.
+    Method parameter does not have a default value.     */
+public class method {
+
+}
